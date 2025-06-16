25227aea78955b651177f478d44592d32c1d00e6
Dinuka Shehan <Shehan.Shyaminda@gmail.com>
2025-06-16 13:45:19 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ afterEvaluate {
 
                 groupId = "com.codelabs"
                 artifactId = "security-kit"
-                version = "1.0.8"
+                version = "1.0.9"
 
                 pom {
                     name.set("iPay Security Kit")
